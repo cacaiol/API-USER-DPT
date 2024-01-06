893ea6187e889eb342ce778da81f3af9adedde2c
Caio <caiobart601@gmail.com>
2024-01-06 17:26:28 -0300

    Adicionando os endpoints

--- src/main/java/Entities/Department.java
+++ src/main/java/Entities/Department.java
@@ -1,11 +1,15 @@
-package entities;
+package Entities;
 
-import jakarta.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_department")
 public class Department {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -1,20 +1,27 @@
-package entities;
+package Entities;
 
-import jakarta.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
 
     @ManyToOne
     @JoinColumn(name = "department_id")
-    private Department department; //ESSE ATRIBUTO É A ASSOCIAÇÃO DAS DUAS ENTIDADES NO MER
+    private Department department;
+
 
     public User() {
     }
